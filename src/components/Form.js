4637cdaa0218d6e9ff38e834cc690513338a5e87
Art Mary <artmary.ir@gmail.com>
2020-04-16 15:43:25 +0430

    using ref in input value or state.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
+  userInput = React.createRef();
+
+  handleSubmit = (event) => {
+    // console.log(this.userInput.current.value);
+    event.preventDefault();
+  };
   render() {
     return (
-      <form action="">
-        <input type="text" placeholder="github username" />
-        <button>Add New Card</button>
+      <form action="" onSubmit={this.handleSubmit}>
+        <input
+          ref={this.userInput}
+          type="text"
+          placeholder="github username"
+          required
+        />
+        <button type="submit">Add New Card</button>
       </form>
     );
   }
