aa1a3693289d3a7a02aea1dbb314afd1ac86a628
Art Mary <artmary.ir@gmail.com>
2020-04-16 15:59:54 +0430

    add new card.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
-  userInput = React.createRef();
-
+  state = {
+    userName: '',
+  };
   handleSubmit = (event) => {
-    // console.log(this.userInput.current.value);
     event.preventDefault();
+    console.log(this.state.userName);
   };
   render() {
     return (
       <form action="" onSubmit={this.handleSubmit}>
         <input
-          ref={this.userInput}
           type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
           placeholder="github username"
           required
         />
