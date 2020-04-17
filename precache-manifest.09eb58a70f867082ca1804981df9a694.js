b7995173855d908150c524e60904a1599e8e195a
Art Mary <artmary.ir@gmail.com>
2020-04-17 17:40:28 +0430

    Updates

---
+++ precache-manifest.09eb58a70f867082ca1804981df9a694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d1a17fee06251e52da9cba38ab7018",
+    "url": "/litti_02/index.html"
+  },
+  {
+    "revision": "0afe8721cb61163ea4b0",
+    "url": "/litti_02/static/css/main.d380089b.chunk.css"
+  },
+  {
+    "revision": "484dd01b3eaa99ef3025",
+    "url": "/litti_02/static/js/2.9b193515.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/litti_02/static/js/2.9b193515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0afe8721cb61163ea4b0",
+    "url": "/litti_02/static/js/main.75f3a08c.chunk.js"
+  },
+  {
+    "revision": "66e6c6755f75428dfc15",
+    "url": "/litti_02/static/js/runtime-main.5272f594.js"
+  }
+]);
